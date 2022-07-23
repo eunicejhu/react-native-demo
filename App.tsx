a194b1709e08ea865fff2ebee8784abb92704a33
ZUOQIN HU (Isabella) <eunicejhu@gmail.com>
2022-07-24 01:21:10 +0200

    feat: add S3 image

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@
  * @format
  */
 
-import React, {type PropsWithChildren} from 'react';
+import React, {type PropsWithChildren, useState} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -17,48 +17,54 @@ import {
   Text,
   useColorScheme,
   View,
+  ImageBackground,
+  Image,
+  Switch,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
 
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    height: 80,
+    transform: [{scale: 0.6}],
+  },
+  imageContainer: {
+    paddingTop: 40,
+  },
+  logo: {
+    height: 200,
+    resizeMode: 'stretch',
+  },
+  theme_container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000c0',
+    height: 60,
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center',
+    paddingLeft: 10,
+  },
+});
+
+const image = {
+  uri: 'https://awss3stack-mybucket15d133bf-qsmiisib9jde.s3.amazonaws.com/zuoqin.pro.png',
+};
+const logo = {
+  uri: 'https://awss3stack-mybucket15d133bf-qsmiisib9jde.s3.amazonaws.com/travel_logo.png',
 };
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [isDarkMode, setIsEnabled] = useState(useColorScheme() === 'dark');
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -70,48 +76,33 @@ const App = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <ImageBackground
+            source={image}
+            resizeMode="cover"
+            style={styles.image}
+          />
+          <View style={styles.theme_container}>
+            <Switch
+              trackColor={{false: '#767577', true: '#81b0ff'}}
+              thumbColor={isDarkMode ? '#f5dd4b' : '#f4f3f4'}
+              ios_backgroundColor="#f4f3f4"
+              onValueChange={toggleSwitch}
+              value={isDarkMode}
+            />
+            <Text style={styles.text}>{isDarkMode ? 'Dark' : 'Light'}</Text>
+          </View>
+
+          <View style={styles.imageContainer}>
+            <Image style={styles.logo} source={logo} />
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
